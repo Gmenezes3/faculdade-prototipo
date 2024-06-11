dce7b0541a747ec67c2d5319f5a0bbad66d39795
Menezes <jorge_menezess@hotmail.com>
2024-06-11 02:07:41 -0300

    Update 00.08
    correção de código e adição de css

---
+++ assets/js/typed.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    new Typed('#typewriter-text', {
+        strings: [
+            "Cansado de ficar procurando vaga para estacionar ?",
+            "A DriveS resolve esse problema para você!"
+        ],
+        typeSpeed: 70,
+        backSpeed: 25,
+        startDelay: 500,
+        backDelay: 1000,
+        loop: false,
+        showCursor: true,
+        cursorChar: '|',
+        autoInsertCss: true,
+        onComplete: function (self) {
+            self.cursor.style.display = 'none'; // Esconde o cursor após a digitação
+        }
+    });
+});
